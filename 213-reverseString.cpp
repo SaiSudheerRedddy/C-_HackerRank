10ecba9772bc1776a9e2f02bb4a6e2149ff0daa0
SaiSudheerReddy <62324377+SaiSudheerRedddy@users.noreply.github.com>
2021-10-01 12:57:36 +0530

    Create 213-reverseString.cpp

---
+++ 213-reverseString.cpp
@@ -0,0 +1,16 @@
+// Link: https://leetcode.com/problems/reverse-string/submissions/
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int start = 0;
+        int end = s.size()-1;
+        while(start<end){
+            char temp = s[end];
+            s[end] = s[start];
+            s[start] = temp;
+            start ++;
+            end --;
+        }
+    }
+};
